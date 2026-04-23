62646560a0e477db8229f4a2337aa3896becca28
ALi Moradzade <alimooradzade@gmail.com>
2026-04-23 23:20:50 +0330

    fixed windows state bug

--- Texty/TextyForm.cs
+++ Texty/TextyForm.cs
@@ -266,7 +266,10 @@ namespace Texty
         private void TextyForm_FormClosed(object sender, FormClosedEventArgs e)
         {
             RegFont.Write(fontDialog1.Font);
-            RegSize.Write(Size);
+            if (WindowState == FormWindowState.Normal)
+            {
+                RegSize.Write(Size);
+            }
             RegWindowState.Write(WindowState);
         }
     }
