c813cfb99f4d050a6a0a67ad70686e32fe2b52c3
ALi Moradzade <alimooradzade@gmail.com>
2026-06-25 13:18:43 +0330

    fixed bug

--- Texty/File/FileManager.cs
+++ Texty/File/FileManager.cs
@@ -10,21 +10,21 @@ namespace Texty.File
 {
     internal static class FileManager
     {
-        public static async Task<string> Read(string filePath)
+        public static async Task<string> Read(string path)
         {
             string text;
-            using (StreamReader sr = new StreamReader(filePath))
+            using (StreamReader sr = new StreamReader(path))
             {
                 text = await sr.ReadToEndAsync();
             }
             return text;
         }
 
-        public static async Task Write(string filePath, string whereToWrite)
+        public static async Task Write(string path, string text)
         {
-            using (StreamWriter streamWriter = new StreamWriter(filePath))
+            using (StreamWriter streamWriter = new StreamWriter(path))
             {
-                await streamWriter.WriteLineAsync(whereToWrite);
+                await streamWriter.WriteLineAsync(text);
             }
         }
     }
